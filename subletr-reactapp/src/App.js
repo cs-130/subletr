bb890fe8ec59670f03adf774a7050b388e119a2c
Jakub Hojsan <jakubhojsan@Jakubs-MacBook-Pro.local>
2023-10-20 19:38:16 -0400

    Added cards with MUI with dynamic fields with defaults

--- subletr-reactapp/src/App.js
+++ subletr-reactapp/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import Listing from './components/listing.js';
 function App() {
   const [listings, setListings] = useState([]);
 
@@ -14,10 +14,38 @@ function App() {
       'Cozy Studio with Park View',
       '2BR Home Near the Beach',
       'Modern Loft with City Skyline',
-      'Country House with Big Garden'
-    ];
-
-    const randomListings = Array.from({ length: 5 }).map(() => {
+      'Country House with Big Garden',
+      'Urban Oasis Studio with City View',
+      'Romantic Beachside Bungalow',
+      'Stylish Downtown Loft Near Eateries',
+      'Chic Countryside Cottage with Views',
+      'Elegant Penthouse with Rooftop Pool',
+      'Historical Home in Old Town',
+      'Sleek Minimalist Apartment Downtown',
+      'Vintage Bed and Breakfast with Charm',
+      'Garden View Suite with Private Balcony',
+      'Artistic Retreat Near Galleries',
+      'Sunny Seaside Condo with Pool Access',
+      'Retro Caravan in Hipster District',
+      'Luxe Villa with Infinity Pool',
+      'Family-Friendly Home with Play Area',
+      'Countryside Chalet Near Hiking Trails',
+      'Designer Condo with Modern Amenities',
+      'Tranquil Getaway with Zen Garden',
+      'Bright Top Floor Flat with Terrace',
+      'Nautical-Themed Boat House',
+      'Treehouse with Panoramic Forest Views',
+      'Contemporary Studio in Art District',
+      'Farmhouse Retreat with Animal Tours',
+      'Lakeside Lodge with Boat Rentals',
+      'Historical Manor with Secret Garden',
+      'Downtown Duplex Steps from Nightlife',
+      'Serene Sanctuary with Meditation Room',
+      'Rustic Ranch with Horseback Riding',
+      'Majestic Castle Overlooking the Vale',
+      ];    
+        
+      const randomListings = Array.from({ length: 30 }).map(() => {
       const randomDescription = sampleDescriptions[Math.floor(Math.random() * sampleDescriptions.length)];
       const randomPrice = Math.floor(Math.random() * (200 - 50 + 1) + 50);
       return {
@@ -35,21 +63,26 @@ function App() {
         Subletr
         <button className="profile-btn">Profile</button>
       </div>
+  
       <div className="search-bar">
         <input type="text" placeholder="Search sublets..." />
         <button>Search</button>
       </div>
-
-      {listings.map((listing, index) => (
-        <div className="listing" key={index}>
-          <h3>{listing.description}</h3>
-          <p>2 guests · 1 bedroom · 1 bed · 1 bath</p>
-          <p>${listing.price}/night</p>
-        </div>
-      ))}
+  
+      <div className="listings-grid">
+        {listings.map((listing, index) => (
+          <Listing
+            key={index}
+            title={listing.description}
+            description="2 guests · 1 bedroom · 1 bed · 1 bath"
+            price={`$${listing.price}/night`}
+          />
+        ))}
+      </div>
     </div>
-
   );
+  
 }
 
 export default App;
+
