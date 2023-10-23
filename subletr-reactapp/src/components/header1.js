edb732f3714a3e2b1f498e590c5b9fa4920c9db0
Jakub Hojsan <jakubhojsan@s-164-67-210-209.resnet.ucla.edu>
2023-10-23 10:54:54 -0700

    Filter added frontend

--- subletr-reactapp/src/components/header1.js
+++ subletr-reactapp/src/components/header1.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Box, Typography, Button, IconButton, TextField } from '@mui/material';
+import { Box, Typography, Button, IconButton, TextField, Icon, InputAdornment } from '@mui/material';
 import Breadcrumbs from './breadcrumbs.js';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import FilterListIcon from '@mui/icons-material/FilterList';
 import Logo from '../images/logo.png';
-import InputAdornment from '@mui/material/InputAdornment';
+import FilterComponent from './filter.js';
+import { useState } from 'react';
+
+
+
 
 
 function HeaderComponent() {
+        const [isFilterVisible, setIsFilterVisible] = useState(false);
+      
+        const toggleFilter = () => { // Toggle filter visibility
+          setIsFilterVisible(prevState => !prevState);
+        };
+
   return (
     <Box className="header" display="flex" flexDirection="column" px={3} py={2}>
-      <Box display="flex" justifyContent="space-between" alignItems="flex-start" width="100%"> {/* Adjusted alignItems here */}
+      <Box display="flex" justifyContent="space-between" alignItems="flex-start" width="100%">
         <Box display="flex" alignItems="center">
           <img src={Logo} alt="Subletr Logo" className="logo" />
         </Box>
@@ -29,19 +40,30 @@ function HeaderComponent() {
                         <SearchIcon color="action" />
                     </InputAdornment>
                     ),
+                    endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton edge="end" aria-label="filter" onClick={toggleFilter}>
+                            <FilterListIcon />
+                        </IconButton>
+                    </InputAdornment>
+                    ),
                 }}
             />
           </Box>
         </Box>
         
+
+
         <Box display="flex" alignItems="center" gap={2}>
-          <Button variant="outlined"sx={{ borderColor: 'green', color: 'green'}}>Profile</Button>
+          <Button variant="outlined" sx={{ borderColor: 'green', color: 'green' }}>Profile</Button>
           <IconButton>
             <MenuIcon />
           </IconButton>
         </Box>
       </Box>
+      {isFilterVisible && <FilterComponent />}
     </Box>
+    
   );
 }
 
