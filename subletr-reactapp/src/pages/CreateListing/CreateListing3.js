b94f524568d3e5345b8a0e21ac206672322f165e
Jakub Hojsan <jakubhojsan@s-164-67-211-193.resnet.ucla.edu>
2023-11-15 16:13:29 -0800

    All create listing pages done reasonably

--- subletr-reactapp/src/pages/CreateListing/CreateListing3.js
+++ subletr-reactapp/src/pages/CreateListing/CreateListing3.js
@@ -1,31 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import Button from '@mui/material/Button'; 
-import DatePicker from 'react-datepicker'; 
 import 'react-datepicker/dist/react-datepicker.css'; 
 import './createListing.css';
 import { useProgress } from './context.js';
 import ProgressBar from './progressBar.js'; 
 import InputBox from './inputBox';
-import DropdownBox from './selectBox';
 
 export default function CreateListing() {
     const { setProgress } = useProgress();
     const navigate = useNavigate(); 
-    const [startDate, setStartDate] = useState(new Date()); 
 
     useEffect(() => {
-        setProgress(40);
+        setProgress(60);
     }, [setProgress]);
 
     const handleNext = () => {
         // Navigate to the next page
-        navigate('/listings/create/3');
+        navigate('/listings/create/4');
     };
 
     const handleBack = () => {
         // Navigate to the previous page
-        navigate('/listings/create/1');
+        navigate('/listings/create/2');
     };
 
     return (
@@ -36,20 +33,14 @@ export default function CreateListing() {
                     <div className='inputs'>
                         <InputBox 
                             className="inputbox" 
-                            label="Upload four images of your place, please!" 
+                            label="Where are you subletting?" 
                             labelStyle={{ fontFamily: 'Roboto, sans-serif', fontSize: '24px' }}
                         />
-                        {/* Date Picker input */}
-                        <div style={{ marginBottom: '50px' }}>
-                            <label style={{ fontFamily: 'Roboto, sans-serif', fontSize: '24px', display: 'block' }}>
-                                Select dates a tenant could stay.
-                            </label>
-                            <DatePicker
-                                selected={startDate}
-                                onChange={(date) => setStartDate(date)}
-                                className="dates"
-                            />
-                        </div>
+                        <InputBox 
+                            className="inputbox" 
+                            label="What features are present at the unit?" 
+                            labelStyle={{ fontFamily: 'Roboto, sans-serif', fontSize: '24px' }}
+                        />
                         
                         {/* Navigation buttons */}
                         <div className='navigationButtons'>
