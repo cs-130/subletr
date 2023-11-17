f316b0e35d87d5aaa619be1ed69fcb6e37f17296
Keaton Heisterman <75706707+keats720@users.noreply.github.com>
2023-11-16 17:59:31 -0800

    adding more to create-listings page

--- subletr-reactapp/src/pages/CreateListing/CreateListing.js
+++ subletr-reactapp/src/pages/CreateListing/CreateListing.js
@@ -41,10 +41,19 @@ export default function CreateListing() {
                                 { label: 'Whole Accomodation', value: '3' },
                             ]}
                         />
-                        <InputBox 
-                            className="inputbox" 
-                            label="How many heads are you subletting for?" 
+                        <DropdownBox 
+                            className="selectGeneral"
+                            label="How many people are you looking to subletting to?"
                             labelStyle={{ fontFamily: 'Roboto, sans-serif', fontSize: '24px' }}
+                            options={[
+                                { label: '1', value: '1' },
+                                { label: '2', value: '2' },
+                                { label: '3', value: '3' },
+                                { label: '4', value: '4' },
+                                { label: '5', value: '5' },
+                                { label: '6', value: '6' },
+                                { label: '>=7', value: '7' },
+                            ]}
                         />
                         <InputBox 
                             className="inputbox" 
