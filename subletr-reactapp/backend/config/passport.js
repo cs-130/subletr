56435efb907b25cbab3f548c68958d5662b9ff72
Keaton Heisterman <75706707+keats720@users.noreply.github.com>
2023-12-08 15:14:45 -0800

    changes

--- subletr-reactapp/backend/config/passport.js
+++ subletr-reactapp/backend/config/passport.js
@@ -5,8 +5,8 @@ module.exports = function (passport) {
   passport.use(
     new GoogleStrategy(
       {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientID: process.env.SERVER_ENV == 'production' ? process.env.GOOGLE_PROD_CLIENT_ID : process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.SERVER_ENV == 'production' ? process.env.GOOGLE_PROD_CLIENT_SECRET : process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
