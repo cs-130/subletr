08ac1227fe8a7eefe5d599255ac92e96e45b7d07
hritikarya <hritikarya1402@gmail.com>
2023-12-08 14:27:41 -0800

    unit tests added

--- subletr-reactapp/backend/tests/listings.test.js
+++ subletr-reactapp/backend/tests/listings.test.js
@@ -1,17 +1,32 @@
 const {
+  editListing,
   getAllListings,
   getUserListings,
   getRentalHistory,
   logListingFavorite,
+  deleteListing,
 } = require("../controllers/listings");
 const Listing = require("../models/Listing");
 const RentalHistory = require("../models/RentalHistory");
 const ListingViewLog = require("../models/ListingViewLog");
+const mongoose = require("mongoose");
+const { describe } = require("node:test");
 
 jest.mock("../models/Listing", () => ({
   find: jest.fn().mockReturnThis(),
   lean: jest.fn(),
+  findOneAndUpdate: jest.fn(),
+  deleteOne: jest.fn(),
 }));
+jest.mock("mongoose", () => {
+  const actualMongoose = jest.requireActual("mongoose"); // Import the actual Mongoose module
+  return {
+    ...actualMongoose, // Spread all of its properties
+    Types: {
+      ObjectId: jest.fn().mockImplementation((id) => id),
+    },
+  };
+});
 
 describe("getAllListings", () => {
   it("should return all listings", async () => {
@@ -73,40 +88,93 @@ describe("getUserListings", () => {
   });
 });
 
-jest.mock("../models/RentalHistory", () => ({
-  find: jest.fn(),
-}));
+describe("deleteListing", () => {
+  it("should successfully delete a listing", async () => {
+    const fakeId = "some-listing-id";
+    const req = { body: { listing_id: fakeId } };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    Listing.deleteOne.mockResolvedValue({ deletedCount: 1 });
 
-describe("getRentalHistory", () => {
-  it("should return rental history for a user", async () => {
-    const mockRentalHistory = [{ id: 1, title: "Rented Listing" }];
-    RentalHistory.find.mockResolvedValue(mockRentalHistory);
+    await deleteListing(req, res);
 
-    const req = { params: { userId: "user1" } };
-    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+    expect(mongoose.Types.ObjectId).toHaveBeenCalledWith(fakeId);
+    expect(Listing.deleteOne).toHaveBeenCalledWith({ _id: fakeId });
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ message: "success" });
+  });
 
-    await getRentalHistory(req, res);
+  it("should handle errors when deleting a listing", async () => {
+    const req = { body: { listing_id: "some-listing-id" } };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    const error = new Error("Deletion failed");
+    Listing.deleteOne.mockRejectedValue(error);
 
-    expect(RentalHistory.find).toHaveBeenCalledWith({ customerId: "user1" });
-    expect(res.json).toHaveBeenCalled();
+    await deleteListing(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: `error in fetching rented listings: ${error}`,
+    });
   });
 });
 
-jest.mock("../models/ListingViewLog", () => {
-  return jest.fn().mockImplementation(() => ({
-    save: jest.fn().mockResolvedValue("saved"),
-  }));
-});
+describe("editListing", () => {
+  it("should successfully edit a listing", async () => {
+    let req = {
+      body: {
+        listing_id: "some-listing-id",
+        address: "478 Midvale Ave, Los Angeles, CA, USA",
+        rent: 2000,
+        availSpots: 1,
+        listingType: "Whole Unit",
+        listingPictures: ["image_1", "image_2"],
+        phoneNumber: 4242484456,
+        amenities: ["swimming", "air conditioner"],
+        bio: "A short bio",
+        description: "Some description",
+        startDate: new Date(),
+        endDate: new Date().setDate(new Date().getDate() + 365),
+        _id: "some-listing-id",
+      },
+    };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    Listing.findOneAndUpdate.mockResolvedValue({ message: "success" });
 
-describe("logListingFavorite", () => {
-  it("should log a listing as favorite", async () => {
-    const req = { body: { listingId: "123", userId: "user1" } };
-    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-    ListingViewLog.mockImplementation(() => ({
-      save: jest.fn().mockResolvedValue("saved"),
-    }));
+    await editListing(req, res);
 
-    const response = await logListingFavorite(req, res);
-    expect(response).toEqual({ message: "success" });
+    expect(mongoose.Types.ObjectId).toHaveBeenCalledWith(req.body.listing_id);
+    expect(Listing.findOneAndUpdate).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ message: "success" });
+  });
+
+  it("should handle errors when editing a listing", async () => {
+    const req = {
+      body: {
+        listing_id: "some-listing-id",
+      },
+    };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    const error = new Error("Update failed");
+    Listing.findOneAndUpdate.mockRejectedValue(error);
+
+    await editListing(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: `error in fetching rented listings: ${error}`,
+    });
   });
 });
